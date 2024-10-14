433ca0b51fb4c73e04e6142b4d8c6758e494fc97
Arseni Aleksandrovich <theintosky@gmail.com>
2024-10-14 19:38:03 +0300

    refactor account class

--- src/Account.java
+++ src/Account.java
@@ -3,9 +3,38 @@ public class Account {
   private String address;
   private double balance;
 
-  public void display(){
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void display(){
       System.out.println(name);
       System.out.println(address);
       System.out.println("Имеется на счету: " + balance);
+
+  }
+
+  public double getInterest (double percentageRate){
+        return balance * percentageRate / 100.00;
   }
 }
