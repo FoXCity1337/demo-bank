7644a2174f0cd9d317ff512220dcf0b6e4a0d9bd
Arseni Aleksandrovich <theintosky@gmail.com>
2024-10-14 19:47:31 +0300

    refactor useAccount  class

--- src/UseAccount.java
+++ src/UseAccount.java
@@ -1,3 +1,5 @@
+
+
 public class UseAccount {
     public static void main(String[] args) {
         Account myAccount = new Account();
@@ -10,13 +12,17 @@ public class UseAccount {
         yourAccount.setBalance(55.63);
         myAccount.display();
         System.out.print("плюс $ ");
+
         System.out.print(myAccount.getInterest(5.00));
+
         System.out.println(" дохода ");
         System.out.println("-----------------");
         yourAccount.display();
         double yourInterestRate = 7.00;
         System.out.print("плюс $ ");
+
         double yourInterestAmount = yourAccount.getInterest(yourInterestRate);
+
         System.out.print(yourInterestAmount);
         System.out.print(" дохода ");
     }
